3dcb871cc112bc085c0a8e6b9662a9ddb40e2231
djambulat <jambulatahmadov99@gmail.com>
2022-09-28 14:56:47 +0300

    created new file db.yaml

--- my-first-project/app.py
+++ my-first-project/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 Bootstrap(app)
 
+# DB configuration
+db = yaml.load(open('db.yaml'))
+
+app.config('MYSQL_HOST') = db['mysql_host']
+app.config('MYSQL_USER') = db['mysql_user']
+app.config('MYSQL_PASSWORD') = db['mysql_password']
+app.config('MYSQL_DB') = db['mysql_db']
+
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
-    cars = ['bmw', 'audi', 'bugatti', 'lamborghini']
-    return render_template('index.html', cars=cars) #cars=cars это передача списка в сам шаблон по ключевому слову cars
+    cursor = mysql.connection.cursor()
+    return render_template('index.html')  # cars=cars это передача списка в сам шаблон по ключевому слову cars
 
 
 @app.route('/about')
